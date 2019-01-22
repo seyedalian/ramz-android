c7cd78eb05576e36361a88445a485c6f9d3e0089
unknown <seyedalian10@gmail.com>
2019-01-22 21:48:42 +0330

    first commit

---
+++ app/src/main/java/sssa/ir/ramz/splash.kt
@@ -0,0 +1,12 @@
+package sssa.ir.ramz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
