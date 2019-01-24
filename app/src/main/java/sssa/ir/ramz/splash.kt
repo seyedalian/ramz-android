6529bee2cd883b7c3052a76995c5506dba6d3db4
unknown <seyedalian10@gmail.com>
2019-01-24 10:38:37 +0330

    first edit splash activity in this commit splash just Transfer to MainActivity

--- app/src/main/java/sssa/ir/ramz/splash.kt
+++ app/src/main/java/sssa/ir/ramz/splash.kt
@@ -1,12 +1,23 @@
 package sssa.ir.ramz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import ir.sssa.esmbazi.ShareReference
 
-class splash : AppCompatActivity() {
+
+class Splash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        if(ShareReference().checkExit(this,FilesName().RAMZE_FILE)){
+
+        }else{
+            startActivity(Intent(this@Splash,MainActivity::class.java))
+        }
+
     }
+
 }
