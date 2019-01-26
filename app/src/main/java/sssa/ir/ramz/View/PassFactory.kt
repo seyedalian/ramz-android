eb51a8548e999f25190e77fd72862d5845e461ec
unknown <seyedalian10@gmail.com>
2019-01-26 16:31:52 +0330

    add passFactory to project

---
+++ app/src/main/java/sssa/ir/ramz/View/PassFactory.kt
@@ -0,0 +1,13 @@
+package sssa.ir.ramz.View
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import sssa.ir.ramz.R
+
+class PassFactory : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pass_factory)
+    }
+}
